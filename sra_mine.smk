957bd6ba0582bdf2bd35e03a26e619699becbb10
mrburke00 <devinburke0@gmail.com>
2021-08-09 16:55:29 -0600

    fixes

--- sra_mine.smk
+++ sra_mine.smk
@@ -8,4 +8,24 @@ rule all:
 	#BAMS AND BAI files
 
 rule download_sra:
+	output:
+		expand('/mnt/local/sra/{type}_1.fastq', type = ['SRR12827888','SRR2131205','SRR12827887']),
+		expand('/mnt/local/sra/{type}_2.fastq', type = ['SRR12827888','SRR2131205','SRR12827887'])
+
+	shell:
+		"bash src/download_sra.sh"
+
+rule generate_reference:
+	output:
+		#reference file?
+	shell:
+
+rule generate_bwa:
+	input:
+		#raw_data directory
+	output:
+		#bams
+	shell:
+
+
 
--- src/download_sra.sh
+++ src/download_sra.sh
@@ -1,11 +1,12 @@
-
-sra_examples=('SRR12827888' 'SRR2131205' 'SRR12827887' 'SRR12460270' 'SRR11521913')
+mkdir /mnt/local/sra
+cd /mnt/local/sra
+sra_examples=('SRR12827888' 'SRR2131205' 'SRR12827887')
 for val1 in ${sra_examples[*]}; do
 	prefetch $val1 --max-size 60g
 	fasterq-dump $val1
-	aws s3 cp ${val1}_1.fastq s3://layerlabcu/test_chicken_sra/
-	aws s3 cp ${val1}_2.fastq s3://layerlabcu/test_chicken_sra/
-	rm -r ${val1}
-	rm -r ${val1}_1.fastq
-	rm -r ${val1}_2.fastq
+	#aws s3 cp ${val1}_1.fastq s3://layerlabcu/test_chicken_sra/
+	#aws s3 cp ${val1}_2.fastq s3://layerlabcu/test_chicken_sra/
+	#rm -r ${val1}
+	#rm -r ${val1}_1.fastq
+	#rm -r ${val1}_2.fastq
 done
