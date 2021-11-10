f33595de3524c6d63155933d84614e2b1c7dd199
mrburke00 <devinburke0@gmail.com>
2021-11-10 11:59:48 -0700

    bugs

--- ncbi_src/generate_sra.py
+++ ncbi_src/generate_sra.py
@@ -16,11 +16,10 @@ df_specific = df.loc[(df['ScientificName'] == organism) & (df['bases'] >= 100000
 df_specific = df_specific.sort_values(by =['bases'], ascending=False)
 
 run_list = df_specific['Run'].to_list()
-print(len(run_list))
+
 with open(file_name+'_sraRuns.txt', 'w') as f:
 	for i in range(0,len(run_list),10):
 		line = ", ".join(run_list[i:i+10])
-		print(line)
 		line += "\n"
 		f.write(line)
 
